f5f7fbb5bc68fd702c1e44fd10d491f9208c7a95
Chris Cannon <chris.c.cannon@gmail.com>
2017-09-20 23:08:36 -0400

    Eliminate a magic number and change var syntax to pythons

--- break_time.py
+++ break_time.py
@@ -1,8 +1,11 @@
 import time
 import webbrowser
 
-timesRun = 0
-while(timesRun<3):
+total_breaks = 3
+times_run = 0
+
+
+while(timesRun<total_breaks):
 	time.sleep(10)
 	webbrowser.open("https://www.youtube.com/watch?v=QZHIxvFgevc")
 	timesRun=timesRun + 1
