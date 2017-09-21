88a939d743f29c08ab08a863b0e77137570dd980
Chris Cannon <chris.c.cannon@gmail.com>
2017-09-20 23:44:34 -0400

    Call function to return a list of all file names

--- SecretMessage/rename_files.py
+++ SecretMessage/rename_files.py
@@ -1,8 +1,11 @@
+import os
 
 #this is my first python function
 def rename_files():
 
 	# 1 get file names from a folder
+	file_list = os.listdir("./prank")
+	print(file_list)
 
 	# 2 rename files without the numbers
 
