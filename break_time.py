4fa3720d1aa7b85b34ca70dc2182cfc6fb12effc
Chris Cannon <chris.c.cannon@gmail.com>
2017-09-20 23:09:39 -0400

    display on the console when the program begins running

--- break_time.py
+++ break_time.py
@@ -4,7 +4,7 @@ import webbrowser
 total_breaks = 3
 times_run = 0
 
-
+print("This program started at "+time.ctime())
 while(timesRun<total_breaks):
 	time.sleep(10)
 	webbrowser.open("https://www.youtube.com/watch?v=QZHIxvFgevc")
